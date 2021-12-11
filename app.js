8b718572271eddfecaddaae04b0d39105466194d
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2021-12-10 23:51:02 -0500

    added data validation

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
+const fs = require('console');
 const inquirer = require('inquirer');
+const generatePage = require('rxjs/internal/testing/ColdObservable');
 
 const promptUser = () => {
     return inquirer.prompt([{
                 type: 'input',
                 name: 'name',
-                message: 'What is your name?'
+                message: 'What is your name? (required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
             },
             {
                 type: 'input',
                 name: 'github',
-                message: 'Enter your GitHub Username',
+                message: 'Enter your GitHub Username (required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your Github name!');
+                    return false;
+                }
+            }
+            },
+            {
+                type: 'confirm',
+                name:'confirmAbout',
+                message: 'Would you like to enter some  about yourself for an "About" section?',
+                default: true
             },
             {
                 type: 'input',
                 name: 'about',
                 message: 'Provide some information about yourself:',
+                when: ({ confirmAbout }) => {
+                    if (confirmAbout) {
+                    return true;
+                    } else {
+                        return false;
+                    }
+                }
             }
         ])
 };
@@ -33,12 +64,28 @@ const promptProject = portfolioData => {
     return inquirer.prompt([{
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?',
+            message: 'What is the name of your project (required)?',
+            validate: nameInput => {
+                if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the project name!');
+                return false;
+            }
+        }
         },
         {
             type: 'input',
             name: 'description',
             message: 'Provide a description of the project (required)',
+            validate: nameInput => {
+                if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a description!');
+                return false;
+            }
+        }
         },
         {
             type: 'checkbox',
@@ -50,6 +97,14 @@ const promptProject = portfolioData => {
             type: 'input',
             name: 'link',
             message: 'Enter the Github link to your project. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a link!');
+                return false;
+            }
+        }
         },
         {
             type: 'confirm',
